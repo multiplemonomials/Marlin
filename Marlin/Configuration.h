06bbfbfdcf8258e04730571e6a4ca9df039be34d
Randall Smith <rsmith@crackofdawn.onmicrosoft.com>
2015-07-26 14:14:58 -0700

    Update steps_per_mm for new extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -481,7 +481,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {52.7413, 106.092, 2267.507, 949.3371}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {52.7413, 106.092, 2267.507, 848.570}
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
