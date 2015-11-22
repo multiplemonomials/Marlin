728244c84ff0e07459f1fdc2fc47f33b85bb221a
Randall Smith <rsmith@crackofdawn.onmicrosoft.com>
2015-11-21 23:02:25 -0800

    Move bed to D9 to try and work around electrical issue

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -578,7 +578,7 @@
   #endif
 
   #if MOTHERBOARD == 33 || MOTHERBOARD == 35 || MOTHERBOARD == 67 || MOTHERBOARD == 68
-    #define FAN_PIN            9 // (Sprinter config)
+    #define FAN_PIN            8 // (Sprinter config)
   #else
     #define FAN_PIN            4 // IO pin. Buffer needed
   #endif
@@ -645,7 +645,7 @@
     #if MOTHERBOARD == 77
       #define HEATER_BED_PIN     9    // BED
     #else
-      #define HEATER_BED_PIN     8    // BED
+      #define HEATER_BED_PIN     9    // BED
     #endif
   #endif
 
